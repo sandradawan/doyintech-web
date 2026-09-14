3745ec95a0d8c800dd76f7e416309e16ba184f8c
SILAS JONATHAN <doyintechnology@gmail.com>
2026-09-14 23:08:21 +0100

    Merge wave3 ebooks into ALL_EBOOKS catalog

--- lib/ebooks-catalog.ts
+++ lib/ebooks-catalog.ts
@@ -1,8 +1,9 @@
 import { EBOOKS } from "./ebooks";
 import { MORE_EBOOKS } from "./ebooks-more";
+import { WAVE3_EBOOKS } from "./ebooks-wave3";
 import type { Ebook } from "./ebooks";
 
-export const ALL_EBOOKS: Ebook[] = [...EBOOKS, ...MORE_EBOOKS];
+export const ALL_EBOOKS: Ebook[] = [...EBOOKS, ...MORE_EBOOKS, ...WAVE3_EBOOKS];
 
 export function findAnyEbook(idOrSlug: string): Ebook | undefined {
   return ALL_EBOOKS.find((e) => e.slug === idOrSlug || e.id === idOrSlug);
